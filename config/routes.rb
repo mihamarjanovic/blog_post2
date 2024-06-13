83382baccea6ee2bd997a06194e388d5988bb2b7
mihamarjanovic <marjanovic.galaxy@gmail.com>
2024-06-13 10:42:06 +0200

    gotovo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :blog_posts do
-    resources :cover_image, only: [:destroy], module: :blog_posts
+    resource :cover_image, only: [:destroy], module: :blog_posts
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
